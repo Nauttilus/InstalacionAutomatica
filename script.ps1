435ed2284651cd7cfcb65d5bbc3f3df0c23354e7
Natt <79367492+Nauttilus@users.noreply.github.com>
2025-06-07 19:52:57 +0200

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,38 @@
+# Google Chrome
+winget install Google.Chrome --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# WizTree (para ver espacio en disco)
+winget install AntibodySoftware.WizTree --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# Discord
+winget install Discord.Discord --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# 7-Zip (mejor que WinRAR)
+winget install 7zip.7zip --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# VLC Media Player
+winget install VideoLAN.VLC --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# WhatsApp
+winget install 9NKSQGP7F2NH --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# GeForce Experience
+winget install Nvidia.GeForceExperience --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# Logitech G HUB
+winget install Logitech.GHUB --no-upgrade --accept-package-agreements --accept-source-agreements
+
+# Steam
+winget install Valve.Steam --accept-package-agreements --accept-source-agreements
+
+# Epic Games Launcher
+winget install EpicGames.EpicGamesLauncher --accept-package-agreements --accept-source-agreements
+
+# Visual Studio Code (VSCode)
+winget install Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+
+# GitHub Desktop
+winget install GitHub.GitHubDesktop --accept-package-agreements --accept-source-agreements
+
+# AMD Radeon Software Adrenalin Edition
+winget install AMD.RadeonSoftware --accept-package-agreements --accept-source-agreements
